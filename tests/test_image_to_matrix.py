e49882fc0b626ab5d9666ef8e6578068eef20859
Karantooo <ctomasalvarezn@gmail.com>
2024-10-03 17:26:08 -0300

    test: se corrigen algunos tests y se añade test para verificar distinto threshold

--- tests/test_image_to_matrix.py
+++ tests/test_image_to_matrix.py
@@ -7,37 +7,33 @@ class TestImageToMatrix:
         pass
 
     def test_creation_matrix_3x3_black(self):
-        imagen_matrix = ImageToMatrix(r"../assets/3X3_black_and_white.png")
-        imagen_matrix.generate_image()
-        expected = np.zeros((3, 3))
-        expected[1, 1] = expected[2, 0] = 1
+        imagen_matrix = ImageToMatrix(r"../assets/3X3_black_and_white.png", 3)
+        expected = np.zeros((3, 3), dtype=bool)
+        expected[1, 1] = expected[2, 0] = True
         generated = imagen_matrix.show_matrix()
         assert np.array_equal(expected, generated)
 
 
     def test_creation_matrix_2x2(self):
-        imagen_matrix = ImageToMatrix(r"../assets/2X2.png")
-        imagen_matrix.generate_image()
-        expected = np.zeros((2, 2))
-        expected[0, 1] = expected[0, 0] = 1
+        imagen_matrix = ImageToMatrix(r"../assets/2X2.png", 2)
+        expected = np.zeros((2, 2), dtype=bool)
+        expected[0, 1] = expected[0, 0] = True
         generated = imagen_matrix.show_matrix()
         assert np.array_equal(expected, generated)
 
     def test_creation_matrix_3x2(self):
-        imagen_matrix = ImageToMatrix(r"../assets/3X2.png")
-        imagen_matrix.generate_image()
-        expected = np.zeros((3, 2))
-        expected[1,1] = expected[1, 0] = 1
+        imagen_matrix = ImageToMatrix(r"../assets/3X2.png", 3, thresh_hold=0.6)
+        expected = np.zeros((3, 2), dtype=bool)
+        expected[1,1] = expected[1, 0] = True
         generated = imagen_matrix.show_matrix()
         assert np.array_equal(expected, generated)
 
     def test_creation_matrix_3x4(self):
-        imagen_matrix = ImageToMatrix(r"../assets/3X4.png")
-        imagen_matrix.generate_image()
-        expected = np.zeros((4, 3))
-        expected[1, 0] = expected[0, 0] = 1
-        expected[0, 2] = expected[1, 2] = 1
-        expected[3, 1] = 1
+        imagen_matrix = ImageToMatrix(r"../assets/3X4.png", 3)
+        expected = np.zeros((4, 3), dtype=bool)
+        expected[1, 0] = expected[0, 0] = True
+        expected[0, 2] = expected[1, 2] = True
+        expected[3, 1] = True
         generated = imagen_matrix.show_matrix()
         assert np.array_equal(expected, generated)
 
