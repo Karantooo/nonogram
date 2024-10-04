b72034a711a2e3896626f04cf3136309619c7b67
karantooo <ctomasalvarezn@gmail.com>
2024-10-04 17:04:18 -0300

    refactor: se refactoriza una ruta en el test y se cambia la ruta de las imagenes de test

--- tests/test_image_to_matrix.py
+++ tests/test_image_to_matrix.py
@@ -5,10 +5,10 @@ import numpy as np
 
 class TestImageToMatrix:
     def setup_method(self):
-        pass
+        self.carpeta_de_assets = r"../assets/image_to_matrix_test/"
 
     def test_creation_matrix_3x3_black(self):
-        imagen_matrix = ImageToMatrix(r"../assets/3X3_black_and_white.png", 3)
+        imagen_matrix = ImageToMatrix(self.carpeta_de_assets + r"3X3_black_and_white.png", 3)
         expected = np.zeros((3, 3), dtype=bool)
         expected[2, 1] = expected[2, 2] = True
         expected[1, 1] = True
@@ -16,23 +16,22 @@ class TestImageToMatrix:
         generated = imagen_matrix.show_matrix()
         assert np.array_equal(expected, generated)
 
-
     def test_creation_matrix_2x2(self):
-        imagen_matrix = ImageToMatrix(r"../assets/2X2.png", 2)
+        imagen_matrix = ImageToMatrix(self.carpeta_de_assets + r"2X2.png", 2)
         expected = np.zeros((2, 2), dtype=bool)
         expected[0, 1] = expected[0, 0] = True
         generated = imagen_matrix.show_matrix()
         assert np.array_equal(expected, generated)
 
     def test_creation_matrix_3x2(self):
-        imagen_matrix = ImageToMatrix(r"../assets/3X2.png", 3, thresh_hold=0.6)
+        imagen_matrix = ImageToMatrix(self.carpeta_de_assets + r"3X2.png", 3, thresh_hold=0.6)
         expected = np.zeros((3, 2), dtype=bool)
         expected[1,1] = expected[1, 0] = True
         generated = imagen_matrix.show_matrix()
         assert np.array_equal(expected, generated)
 
     def test_creation_matrix_3x4(self):
-        imagen_matrix = ImageToMatrix(r"../assets/3X4.png", 3)
+        imagen_matrix = ImageToMatrix(self.carpeta_de_assets + r"3X4.png", 3)
         expected = np.zeros((4, 3), dtype=bool)
         expected[1, 0] = expected[0, 0] = True
         expected[0, 2] = expected[1, 2] = True
