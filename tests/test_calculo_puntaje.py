0131820fba504e5a8e6461acad9436ca5757ae61
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2024-10-05 19:44:33 -0300

    feat: se agrego un test mas para los test de puntaje, q da un valor erroneo, q se deve corregir en el siguente commit

--- tests/test_calculo_puntaje.py
+++ tests/test_calculo_puntaje.py
@@ -12,7 +12,6 @@ def test_puntaje_1():
     assert puntaje == 8400
 
 def test_puntaje_2():
-    #prueba q el calculo del puntaje sea correcto(ttd)
     tiempo = timedelta(seconds= 60)
     dificultad = 1
     vidas = 1
@@ -21,11 +20,17 @@ def test_puntaje_2():
     assert puntaje == 3600
 
 def test_puntaje_3():
-    #prueba q el calculo del puntaje sea correcto(ttd)
-    tiempo = timedelta(seconds= 300)
+    tiempo = timedelta(seconds= 90)
     dificultad = 1
     vidas = 0
     calculador = calculador_puntaje(int(tiempo.total_seconds()), vidas, dificultad)
     puntaje  = calculador.calcular()
     assert puntaje == 0
 
+def test_puntaje_4():
+    tiempo = timedelta(seconds= 300)
+    dificultad = 3
+    vidas = 2
+    calculador = calculador_puntaje(int(tiempo.total_seconds()), vidas, dificultad)
+    puntaje = calculador.calcular()
+    assert puntaje == 0
