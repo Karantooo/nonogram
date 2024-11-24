0e1179a5e0f52998c1838f9f5bf5394a375da954
AntoCreed777 <abenavides@desem.cl>
2024-11-24 19:14:06 -0300

    refactor: Cambio en la animacion de Derrota

--- nonogram/visual/animacion_derrota.py
+++ nonogram/visual/animacion_derrota.py
@@ -11,6 +11,8 @@ class AnimacionDerrota(InterfazAnimacionVD):
     gif_size: tuple[int,int]
 
     orden_impresion: bool
+    alpha: int
+    alpha_incremento: int
 
     def __init__(self, screen: pygame.Surface):
         self.screen = screen
@@ -18,9 +20,9 @@ class AnimacionDerrota(InterfazAnimacionVD):
         screen_width, screen_height = pygame.display.Info().current_w, pygame.display.Info().current_h
         self.posicion = (int(screen_width / 2), int(screen_height / 2))
 
-        self.gif_size = (screen_height,  screen_height)
+        self.gif_size = (screen_width,  screen_height)
 
-        gif = Image.open("assets/gif_derrota.gif")
+        gif = Image.open("assets/gif_game_over.gif")
 
         self.frames = []
 
@@ -33,11 +35,23 @@ class AnimacionDerrota(InterfazAnimacionVD):
 
         self.frame_index = len(self.frames) - 1
         self.orden_impresion = False
+        self.alpha = 0
+        self.alpha_incremento = 7
 
     def imprimir(self) -> None:
         x = self.posicion[0] - self.gif_size[0] / 2
         y = self.posicion[1] - self.gif_size[1] / 2
-        self.screen.blit(self.frames[self.frame_index], (x, y))
+
+        # Incrementar el alpha hasta el máximo (255)
+        if self.alpha < 255:
+            self.alpha += self.alpha_incremento
+        if self.alpha > 255:
+            self.alpha = 255  # Limitar el valor máximo
+
+        # Aplicar el alpha al frame actual
+        frame = self.frames[self.frame_index].copy()  # Copia para aplicar transparencia
+        frame.set_alpha(self.alpha)
+        self.screen.blit(frame, (x, y))
 
         pygame.display.flip()
 
